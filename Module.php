4cc64057982e98278a76aa30fec5662dab408c71
Daniel Berthereau <Daniel.git@Berthereau.net>
2020-09-28 00:00:00 +0000

    Initial files.

---
+++ Module.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace DerivativeMedia;
+
+if (!class_exists(\Generic\AbstractModule::class)) {
+    require file_exists(dirname(__DIR__) . '/Generic/AbstractModule.php')
+        ? dirname(__DIR__) . '/Generic/AbstractModule.php'
+        : __DIR__ . '/src/Generic/AbstractModule.php';
+}
+
+use Generic\AbstractModule;
+
+/**
+ * Derivative Media
+ *
+ * Create derivative audio/video media files for cross-browser compatibility.
+ *
+ * @copyright Daniel Berthereau, 2020
+ * @license http://www.cecill.info/licences/Licence_CeCILL_V2.1-en.txt
+ */
+class Module extends AbstractModule
+{
+    const NAMESPACE = __NAMESPACE__;
+
+    protected $dependency = 'Log';
+
+    public function getConfig()
+    {
+        return include __DIR__ . '/config/module.config.php';
+    }
+}
---
+++ config/module.config.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace DerivativeMedia;
+
+return [
+    'translator' => [
+        'translation_file_patterns' => [
+            [
+                'type' => 'gettext',
+                'base_dir' => dirname(__DIR__) . '/language',
+                'pattern' => '%s.mo',
+                'text_domain' => null,
+            ],
+        ],
+    ],
+    'derivativemedia' => [
+        'settings' => [
+        ],
+    ],
+];
