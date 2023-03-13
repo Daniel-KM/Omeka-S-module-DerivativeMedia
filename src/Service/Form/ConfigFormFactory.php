a9517e51b49906836882fff3133f41a1be8dc14c
Daniel Berthereau <Daniel.git@Berthereau.net>
2023-03-13 00:00:00 +0000

    Fixed form.

--- src/Service/Form/ConfigFormFactory.php
+++ src/Service/Form/ConfigFormFactory.php
@@ -10,7 +10,7 @@ class ConfigFormFactory implements FactoryInterface
 {
     public function __invoke(ContainerInterface $services, $requestedName, array $options = null)
     {
-        $form = new ConfigForm(null, $options);
+        $form = new ConfigForm(null, $options ?? []);
         return $form
             ->setConnection($services->get('Omeka\Connection'));
     }
