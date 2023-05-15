f7cdaef82ace918ee27fe6398de6eaa7d5a7aa27
Daniel Berthereau <Daniel.git@Berthereau.net>
2023-05-15 00:00:00 +0000

    Added a js to manage download links.

---
+++ asset/js/derivative-media.js
@@ -0,0 +1,50 @@
+$(function () {
+
+    $('a.derivative-media.on-demand').on('click', (e) => {
+        e.stopPropagation();
+
+        if ($('#derivative-form').length) {
+           $('#derivative-form').remove();
+        }
+
+        const link = $(this);
+        const url = link.data('url');
+        const size = link.data('size');
+
+        const textWarn = `Are you sure to download the file (${size} bytes)?`
+        const textNo = `No`;
+        const textYes = `Yes`;
+
+        const html=`
+<dialog id="derivative-on-demand">
+    <form id="derivative-form" method="dialog" action="#">
+        <p>${textWarn}</p>
+        <div>
+            <button type="cancel" id="derivative-no" value="no" formmethod="dialog">${textNo}</button>
+            <button type="button" id="derivative-yes" value="yes" formmethod="dialog>${textYes}</button>
+        </div>
+    </form>
+</dialog>`;
+
+        $('body').append(html);
+
+        const dialog = document.getElementById('derivative-on-demand');
+        const no = dialog.getElementById('derivative-no');
+        const yes = dialog.getElementById('derivative-yes');
+
+        dialog.showModal();
+
+        no.addEventListener('click', () => {
+            link.prop('href', '#');
+            dialog.close(false);
+            e.preventDefault();
+        });
+
+        yes.addEventListener('click', () => {
+            link.prop('href', url);
+            dialog.close(true);
+        });
+
+    });
+
+});
